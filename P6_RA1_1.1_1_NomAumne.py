b8644fffeafe0e6ba63579a2997bd7eb68bae4fb
JoelGarciaMartinez <joelgarciamartinez2005@gmail.com>
2025-04-25 09:45:50 +0200

    Delete P6_RA1_1.1_1_NomAumne.py

--- P6_RA1_1.1_1_NomAumne.py
+++
@@ -1,9 +0,0 @@
-# --------------------------------------------------
-# Nom del fitxer: P6_RA1_1.1_1_NomAumne.py
-# Autor: Joel
-# Data de creació: 24/04/2025
-# Descripció: Creem una variable on almacena el meu nom i despres en diu amb el print
-# --------------------------------------------------
-
-nom = "joel"
-print(nom)
